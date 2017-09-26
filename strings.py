1c5cf41610a5685eae77fe3e451d2014d30ac18a
KittenDeveloper <fuller.justice@gmail.com>
2017-09-26 01:57:40 -0500

    Update and rename strclass.py to strings.py
    -Added multiplication support for both binary/augmented types.
    -Raise NotImplementedError on augmented (__idiv__) division
    -Changed in_str optional argument to val=None
    -Rename

--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
 class String(object):
-	def __init__(self, in_str=None):
-		if in_str!=None:self.set(str(in_str))
+	def __init__(self, val=None):
+		if in_str!=None:self.set(str(val))
 		self.chars=[]
 	def set(self, nstr):
 		self.chars=[]
@@ -13,11 +13,18 @@ class String(object):
 	def __iter__(self):
 		for x in self.chars:
 			yield x
+	def tostr(self):return str(self)
 	def __getitem__(self, key):return self.chars[key]
 	def __setitem__(self, key, value):self.chars[key]=value
 	def __delitem__(self, key):del self.chars[key]
 	def __add__(self, other):return (str(self)+str(other))
 	def __iadd__(self, other):self.concat(str(other))
+	def __mul__(self, other):
+		other=int(other)
+		return str(self)*other
+	def __imul__(self, other):
+		other=int(other)
+		self.set(str(self)*other)
 	def __sub__(self, other):
 		if type(other)!=int:other=int(other)
 		return str(self)[:-other]
@@ -30,6 +37,7 @@ class String(object):
 	def __div__(self, other):
 		other=str(other)
 		return str(self).split(other)
+	def __idiv__(self, other):raise NotImplementedError
 	def __rdiv__(self, other):
 		try:selfnum=eval(str(self))
 		except NameError:raise TypeError("unsupported operand type(s) for /: 'int' and 'str'")
